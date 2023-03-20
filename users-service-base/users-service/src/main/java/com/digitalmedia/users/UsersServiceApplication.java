4cee7acd82944a6f9479c8f995e3781ec17777e6
Victoria Stancic <89276741+VickyStancic@users.noreply.github.com>
2023-03-20 01:37:06 -0300

    Add files via upload

---
+++ users-service-base/users-service/src/main/java/com/digitalmedia/users/UsersServiceApplication.java
@@ -0,0 +1,56 @@
+package com.digitalmedia.users;
+
+import com.digitalmedia.users.model.User;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+public class UsersServiceApplication {
+
+  public static void save(User user, String group){
+    Keycloak keycloak = KeycloakBuilder
+            .builder()
+            .serverUrl("http://localhost:8080/")
+            .realm("DigitalMedia")
+            .clientId("microservicios")
+            .clientSecret("q1XaL3TT2u2WjHkLjx5PWuzEf1t7Q2MQ")
+            .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+            .build();
+
+
+    CredentialRepresentation credentialRepresentationAdmin = new CredentialRepresentation();
+    credentialRepresentationAdmin.setType(CredentialRepresentation.PASSWORD);
+    credentialRepresentationAdmin.setValue(group);
+    UserRepresentation userRepresentationAdmin = new UserRepresentation();
+    userRepresentationAdmin.setUsername(user.getUsername());
+    userRepresentationAdmin.setFirstName(user.getFirstName());
+    userRepresentationAdmin.setLastName(user.getLastName());
+    userRepresentationAdmin.setEnabled(true);
+    userRepresentationAdmin.setGroups(List.of(group));
+    userRepresentationAdmin.setEmail(user.getEmail());
+    userRepresentationAdmin.setCredentials(Arrays.asList(credentialRepresentationAdmin));
+    keycloak.realm("DigitalMedia").users().create(userRepresentationAdmin);
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(UsersServiceApplication.class, args);
+
+    User userAdmin = new User("useradmin", "admin@mail", "adminName", "adminLastName");
+    User userClient = new User("adminLastName", "client@mail", "clientName", "clientLastName");
+    User userProvider = new User("userprovider", "provider@mail", "providerName", "providerLastName");
+
+    save(userAdmin, "admin");
+    save(userClient, "client");
+    save(userProvider, "provider");
+    
+  }
+
+}
---
+++ users-service-base/users-service/src/main/java/com/digitalmedia/users/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.digitalmedia.users.controller;
+
+import com.digitalmedia.users.model.User;
+import com.digitalmedia.users.model.dto.UserDTO;
+import com.digitalmedia.users.service.UserService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+  private UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("/admin")
+  @PreAuthorize("hasAuthority('GROUP_admin')")
+  public List<UserDTO> getUsersNoAdmin() {
+    return userService.findNoAdmin();
+  }
+
+  @GetMapping("/admin/find/{username}")
+  @PreAuthorize("hasAuthority('GROUP_admin')")
+  public User getUserByUsernameAdmin(@PathVariable("username") String username) {
+    return userService.findByUsername(username);
+  }
+
+  @GetMapping("/find/{username}")
+  public User getUserByUsername(@PathVariable("username") String username) {
+    return userService.findByUsername(username);
+  }
+}
